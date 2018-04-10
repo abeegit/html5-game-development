b97c0daafeff7c8b54acdda09024f3d0951691a6
Abhijeet Kumar <abhijeet.kumar@22feettribalww.com>
2018-04-10 19:44:40 +0530

    WIP

--- scripts/main.js
+++ scripts/main.js
@@ -1,112 +1,35 @@
 (function() {
-    var canvas,
-        ctx,
-		spriteSheet,
-        sprites = [],
-        fps = 60,
-		step = 1/fps,
-        frameCount = 0,
-        background;
-
-    /* Polyfills for requestAnimationFrame */
-    if (!window.requestAnimationFrame) {
-        window.requestAnimationFrame =
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function(callback, element) {
-                window.setTimeout(callback, 1000 / 60);
-            };
-    }
-
-    canvas = document.getElementById("game");
-    ctx = canvas.getContext("2d");
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    background = new Image();
-    background.onload = function() { Game.render() };
-    background.src = "./assets/images/race_background.jpg";
-
-    var Util = {
-        timestamp: function() { return new Date().getTime() }
+    var vw = window.innerWidth;
+    var vh = window.innerHeight;
+    var backgroundSource = {
+        name: "./assets/images/race_background.jpg",
+        width: 1024,
+        height: 16384
     };
 
-    var Game = {
-        render() {
-            ctx.drawImage(background, 0, 19882 - canvas.height, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
-            setTimeout(Game.run, 2000);
-        },
-        run: function(options) {
-            console.log("running");
-            var vx = 0;
-            function moveBike() {
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.drawImage(background, 0, 19882 - (canvas.height + vx), canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
-                requestAnimationFrame(moveBike);
-            };
-            requestAnimationFrame(moveBike);
-        },
-        moveBike() {
+    /** Creates a canvas with `vw` width and `vh` height and appends to the body */
+    var App = new PIXI.Application(vw, vh),
+        loader = PIXI.loader,
+        resouces = PIXI.resources,
+        Sprite = PIXI.Sprite; 
+    document.body.appendChild(App.view);
 
-        }
+    loader
+        .add("bg", backgroundSource.name)
+        .on("progress", loadProgressHandler)
+        .load(setup);
+
+    function setup() {
+        var background = new Sprite(PIXI.loader.resources.bg.texture);
+        background.x = 0;
+        background.y = backgroundSource.height - vh;
+        background.width = 1024;
+        background.height = 16384;
+        App.stage
+            .addChild(background);
     };
 
-	function drawImage() {
-        // var 
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		var dimensions = sprites[frame % frames.length];
-		context.drawImage(
-			spriteSheet,
-			dimensions.x,
-			dimensions.y,
-			dimensions.w,
-			dimensions.h,
-			60 + dimensions.cx,
-			80 + dimensions.cy,
-			dimensions.w,
-			dimensions.h
-		);
-        frame++;
-        requestAnimationFrame(drawImage);
-	}
-
-	function defineSprites() {
-		for (var i = 0; i < frames.length; i++) {
-			var spriteRaw = frames[i];
-			var framesRaw = spriteRaw.frame;
-			var cx, cy;
-			var trimmed = frames[i].trimmed;
-
-			if (trimmed) {
-				cx =
-					spriteRaw.spriteSourceSize.x - spriteRaw.sourceSize.w * 0.5;
-				cy =
-					spriteRaw.spriteSourceSize.y - spriteRaw.sourceSize.h * 0.5;
-			} else {
-				cx = -spriteRaw.w * 0.5;
-				cy = -spriteRaw.h * 0.5;
-			}
-
-			var sprite = {
-				name: spriteRaw.filename,
-				x: framesRaw.x,
-				y: framesRaw.y,
-				w: framesRaw.w,
-				h: framesRaw.h,
-				cx: cx,
-				cy: cy
-			};
-			sprites.push(sprite);
-		}
-		animate();
-	}
-
-	function animate() {
-		requestAnimationFrame(drawImage);
+    function loadProgressHandler(loader) {
+        console.log("progress" + loader.progress + "%");
     }
-    
-    //Game.render();
 })();
